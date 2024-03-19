40fdff37d2c165daf9b0b1e98eb31dd575c82115
vanthan04 <thanvantran1707@gmail.com>
2024-03-19 10:46:57 +0700

    config bodyParser

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 require("dotenv").config()
 const express = require("express")
 const connectDB = require("./configs/connectDB")
-
+const bodyParser = require("body-parser")
 connectDB()
 const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 const port = process.env.PORT || 6000
 app.listen(port, ()=>{
