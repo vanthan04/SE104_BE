fb4e4e201796bfd2893fe65a34a0e1a56fdddabf
Tam Anh Nguyen Duy <nguyenduytamanh03012004@gmail.com>
2024-06-02 22:55:21 +0700

    Update Models DocGia

--- src/models/DocGia.js
+++ src/models/DocGia.js
@@ -1,35 +1,35 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt")
-const {calculateDate} = require("../helps/calculateTime")
+const { calculateDate } = require("../helps/calculateTime")
 
 var DocGiaSchema = new mongoose.Schema(
     {
-        MaDG:{
+        MaDG: {
             type: String,
             required: true,
             unique: true
         },
-        hoten:{
+        hoten: {
             type: String,
-            require:true
+            require: true
         },
-        email:{
+        email: {
             type: String,
             require: true,
             unique: true
         },
-        diachi:{
+        diachi: {
             type: String,
         },
-        loaidocgia:{
+        loaidocgia: {
             type: String,
             enum: ['X', 'Y'],
             require: true
         },
-        ngaysinh:{
+        ngaysinh: {
             type: Date,
         },
-        isLocked:{
+        isLocked: {
             type: Boolean,
             default: false
         },
@@ -37,27 +37,107 @@ var DocGiaSchema = new mongoose.Schema(
             type: String,
             default: null
         },
-        ngaylapthe:{
+        ngaylapthe: {
             type: Date,
             require: true,
         },
-        tongno:{
+        tongno: {
             type: Number,
             default: 0
         },
     },
     {
-      timestamps: true  
+        timestamps: true
     }
 )
 
 
 DocGiaSchema.methods = {
-    updateReader: async function(minAge, maxAge, cardValue){
-        if (calculateDate(this.ngaysinh) < minAge || calculateDate(this.ngaysinh) > maxAge){
+    updateReader: async function (minAge, maxAge, cardValue) {
+        if ((calculateDate(this.ngaysinh) < minAge || calculateDate(this.ngaysinh) > maxAge) && (calculateDate(this.ngaylapthe) > (cardValue / 12) || calculateDate(this.ngaylapthe) < 0)) {
             this.isLocked = true;
-        } else if (calculateDate(this.ngaylapthe) > (cardValue/12) || calculateDate(this.ngaylapthe) < 0){
+            this.reasonLocked = "Sai quy định ngày sinh và ngày lập thẻ!"
+        }
+        else if (calculateDate(this.ngaysinh) < minAge || calculateDate(this.ngaysinh) > maxAge) {
             this.isLocked = true;
+            this.reasonLocked = "Sai quy định ngày sinh!"
+        }
+        else if (calculateDate(this.ngaylapthe) > (cardValue / 12) || calculateDate(this.ngaylapthe) < 0) {
+            this.isLocked = true;
+            this.reasonLocked = "Sai quy định ngày lập thẻ!"
+        } else {
+            this.isLocked = false;
+        }
+        await this.save();
+    }
+}
+module.exports = mongoose.model("DocGia", DocGiaSchema); const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
+const { calculateDate } = require("../helps/calculateTime")
+
+var DocGiaSchema = new mongoose.Schema(
+    {
+        MaDG: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        hoten: {
+            type: String,
+            require: true
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true
+        },
+        diachi: {
+            type: String,
+        },
+        loaidocgia: {
+            type: String,
+            enum: ['X', 'Y'],
+            require: true
+        },
+        ngaysinh: {
+            type: Date,
+        },
+        isLocked: {
+            type: Boolean,
+            default: false
+        },
+        reasonLocked: {
+            type: String,
+            default: null
+        },
+        ngaylapthe: {
+            type: Date,
+            require: true,
+        },
+        tongno: {
+            type: Number,
+            default: 0
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+DocGiaSchema.methods = {
+    updateReader: async function (minAge, maxAge, cardValue) {
+        if ((calculateDate(this.ngaysinh) < minAge || calculateDate(this.ngaysinh) > maxAge) && (calculateDate(this.ngaylapthe) > (cardValue / 12) || calculateDate(this.ngaylapthe) < 0)) {
+            this.isLocked = true;
+            this.reasonLocked = "Sai quy định ngày sinh và ngày lập thẻ!"
+        }
+        else if (calculateDate(this.ngaysinh) < minAge || calculateDate(this.ngaysinh) > maxAge) {
+            this.isLocked = true;
+            this.reasonLocked = "Sai quy định ngày sinh!"
+        }
+        else if (calculateDate(this.ngaylapthe) > (cardValue / 12) || calculateDate(this.ngaylapthe) < 0) {
+            this.isLocked = true;
+            this.reasonLocked = "Sai quy định ngày lập thẻ!"
         } else {
             this.isLocked = false;
         }
