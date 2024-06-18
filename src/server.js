80ba51caa48f59b0079de7257d1e30af8784cb35
vanthan04 <thanvantran1707@gmail.com>
2024-06-18 22:07:51 +0700

    update cors

--- src/server.js
+++ src/server.js
@@ -11,22 +11,17 @@ connectDB()
 const initRoutes = require("./routes/index");
 const app = express()
 
-app.use(cors({
-  origin: 'http://localhost:3000', // Thay bằng domain của bạn nếu khác
-  methods: ["POST", "PUT", "GET", "DELETE"],
-  credentials: true, // Để cho phép gửi cookie kèm theo yêu cầu
-}))
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, process.env.CLIENT_URL_LOCALHOST],
+    methods: ["POST", "PUT", "GET", "DELETE"],
+    credentials: true,
+  })
+);
 
-// app.set('trust proxy', 1);
 
 app.use(cookieParser());
-// app.use(cookieSession(
-//   {
-//     name: 'session',
-//     keys: ['lama'],
-//     maxAge: 24 * 60 * 60 * 100
-//   }
-// ))
+
 app.use(session({
   secret: 'secret',
   saveUninitialized: true,
